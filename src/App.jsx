11173638ac1205b790a7ddbd38b699f6e6157951
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-07-12 19:55:30 -0300

    Colocando un contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
-import "./components/Titulos";
 import Titulos from "./components/Titulos";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Contador from "./components/Contador";
 
 function App() {
   return (
     <Container>
+      {/* <Titulos/> */}
       <Titulos></Titulos>
-      {/* <Titulos /> */}
+      <Contador></Contador>
     </Container>
   );
 }
--- src/components/Titulos.jsx
+++ src/components/Titulos.jsx
@@ -1,13 +1,15 @@
 const Titulos = () => {
-  //Aqui vamos a agregar la logica del componente
+  //aqui vamos a cargar toda la logica
+
   return (
-    //Aqui tambien se puede poner algo de logica
+    // aqui tambien puedo escribir un poco de logica
     <section className="text-center">
-      {/*  */}
-      <h1 className="display-4">Proyecto contador react</h1>
+      {/* aqui va todo el maquetado  */}
+      <h1 className="display-4">Proyecto contador con react</h1>
       <h2 className="display-6">Este es un subtitulo</h2>
       <hr />
     </section>
   );
 };
+
 export default Titulos;
