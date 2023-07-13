1db677bb9dde6b8768dc9ada2bd024176892b033
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-07-12 21:02:48 -0300

    Anadiendo funcion al boton contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 const Contador = () => {
+  const [numero, setNumero] = useState(0); //Necesito poner algo dentro del parentesis para inicializarlo
+  //Esto es para pora que se renderice re modo localizado la variable que cambia su estado.
   return (
     <section>
       <h3>Contador</h3>
-      <h3>10</h3>
-      <Button variant="primary">+1</Button>
+      <h3>{numero}</h3>
+      <Button variant="primary" onClick={() => setNumero(numero + 1)}>
+        +1
+      </Button>
     </section>
   );
 };
